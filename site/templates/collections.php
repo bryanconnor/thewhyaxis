66745c6d8da9a4e4cd10d053f550e816628c17c7
Bryan Connor <bryan.j.connor@gmail.com>
2014-03-25 10:42:54 -0400

    Updates to URLs on the collections index

--- site/templates/collections.php
+++ site/templates/collections.php
@@ -16,7 +16,7 @@
     <div class="flexslider">
       <ul class="slides">
       <li class="collection-hero process">
-        <a href="<?php $site->url()?>process">
+        <a href="<?php echo( $site->url())?>/process">
           <div class="scroll-description">
             <h1>Behind the Scenes</h1>
             <p>Interviews and articles that reveal the design process behind great visualizations.</p>
@@ -24,7 +24,7 @@
         </a>
       </li>
       <li class="collection-hero storytelling">
-        <a href="<?php $site->url()?>storytelling">
+        <a href="<?php echo( $site->url())?>/storytelling">
           <div class="scroll-description">
             <h1>Storytelling With Data</h1>
             <p>Leading the reader through a visualization to impart knowledge and insight.</p>
@@ -32,7 +32,7 @@
         </a>
       </li>
       <li class=" collection-hero print-web">
-        <a href="<?php $site->url()?>print-web">
+        <a href="<?php echo( $site->url())?>/print-web">
           <div class="scroll-description">
             <h1>Print &amp; Web Companions</h1>
             <p>Visualizations covered on The Why Axis with both print and web components that work together.</p>
@@ -40,7 +40,7 @@
         </a>
       </li>
       <li class="collection-hero remakes">
-        <a href="<?php $site->url()?>remakes">
+        <a href="<?php echo( $site->url())?>/remakes">
           <div class="scroll-description">
             <h1>Chart Remakes</h1>
             <p>Articles on The Why Axis and elsewhere that seek to redesign or improve existing visualizations.</p>
@@ -48,7 +48,7 @@
         </a>
       </li>
       <li class="collection-hero tools">
-        <a href="<?php $site->url()?>tools">
+        <a href="<?php echo( $site->url())?>/tools">
           <div class="scroll-description">
             <h1>Tools of the Trade</h1>
             <p>A collection of the data visualization tools reviewed on The Why Axis.</p>
@@ -56,7 +56,7 @@
         </a>
       </li>
       <li class="collection-hero open-source">
-        <a href="<?php $site->url()?>open-source">
+        <a href="<?php echo( $site->url())?>/open-source">
           <div class="scroll-description">
             <h1>Open Source</h1>
             <p>Resources with open source components including visualization libraries, code bases and data sets.</p>
@@ -64,7 +64,7 @@
         </a>
       </li>
       <li class="collection-hero journalism">
-        <a href="<?php $site->url()?>journalism">
+        <a href="<?php echo( $site->url())?>/journalism">
           <div class="scroll-description">
             <h1>Data Journalism</h1>
             <p>Visualizations that tackle major news stories and make them accessible and understandable for the general public.</p>
@@ -72,7 +72,7 @@
         </a>
       </li>
       <li class="collection-hero climate-change">
-        <a href="<?php $site->url()?>climate-change">
+        <a href="<?php echo( $site->url())?>/climate-change">
           <div class="scroll-description">
             <h1>Making Climate Change Visual</h1>
             <p>Visualizations that examine one aspect of the important yet ephemeral issue of climate change.</p>
