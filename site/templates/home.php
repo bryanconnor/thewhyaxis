c09d2baab601efd8898b34584c4da62284f94b64
Bryan Connor <bryan.j.connor@gmail.com>
2014-04-29 10:53:48 -0400

    added proper uri echo

--- site/templates/home.php
+++ site/templates/home.php
@@ -62,7 +62,7 @@
           </a>
       <div class="permalink">
         <span class="permalabel">Copy Permalink:</span> 
-        <input class="permalinkfield" value="http://thewhyaxis.info/visualizing-health"></input>
+        <input class="permalinkfield" value="http://thewhyaxis.info/<?php echo $article->uri() ?>"></input>
       </div>
     </div>
 
